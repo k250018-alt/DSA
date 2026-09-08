96262e7a323219f30d2cccf3bba543fd449bdbc8
k250018-alt <k250018@nu.edu.pk>
2026-09-08 08:33:29 +0500

    Refactor arrange function to improve inner loop index

--- DSA/DS THEORY/TASK 3/Q1.cpp
+++ DSA/DS THEORY/TASK 3/Q1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #include "Singly_ll.h"
 Singly_LL<int> arrange(Singly_LL<int> &list){
     for(int i = 0 ; i < list.get_count() ; i++){
-        for (int  j = 0 ; j < list.get_count()-1 ; j++){
+        for (int  j = i ; j < list.get_count()-1 ; j++){
             if(list.search_Node(j) % 2 != 0 && list.search_Node(j+1)%2 ==0){
                 list.insertinbetween(list.search_Node(j) , j);
                 list.insertinbetween(list.search_Node(j+2),j);
